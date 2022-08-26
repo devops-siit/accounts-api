8146d7edd9dad7ed26d0ddb7390dc29d1d4aa515
Aleksandra Jordanović <ana.jordanovic@hotmail.com>
2022-08-26 21:55:00 +0200

    bug: removed eureka server

--- src/main/java/com/dislinkt/accountsapi/AccountsApiApplication.java
+++ src/main/java/com/dislinkt/accountsapi/AccountsApiApplication.java
@@ -2,10 +2,8 @@ package com.dislinkt.accountsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class AccountsApiApplication {
 
 	public static void main(String[] args) {
